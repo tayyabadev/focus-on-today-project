c38c453e942c8f88700249458272e99a4d22b470
tayyaba <tayyabadev@gmail.com>
2025-09-02 17:18:27 +0500

    change the logic to handle individul tasks without entering others

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const errorMsg = document.querySelector("#error-msg");
 let innerStatusBar = document.querySelector("#inner-status-bar");
 const doneNumber = document.querySelector("#done-number");
 let inspiringTexts = document.querySelector("#inspiring-text");
+let totalGoals = 0;
+let completedGoalsCount = 0;
 let quotes = [
   "Raise the bar by completing your goals!",
   "A journey of a thousand miles begins with a single step",
@@ -13,45 +15,46 @@ let quotes = [
   "Congratulations! Be proud of what you've achieved.",
 ];
 const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
-let completedGoalsCount = Object.values(allGoals).filter(
-  (goal) => goal.completed
-).length;
-innerStatusBar.style.width = `${(completedGoalsCount / 3) * 100}%`;
-doneNumber.textContent = "";
+const progressBarFun = () => {
+  totalGoals = Object.values(allGoals).filter((goal) => goal.name).length;
+  completedGoalsCount = Object.values(allGoals).filter(
+    (goal) => goal.completed
+  ).length;
+  innerStatusBar.style.width = `${(completedGoalsCount / totalGoals) * 100}%`;
+  doneNumber.textContent = `${completedGoalsCount}/${totalGoals} completed`;
+};
+
+progressBarFun();
+
 circles.forEach((circle, i) => {
   circle.addEventListener("click", () => {
-    const allfieldFilled = taskTextArray.every((task) => {
-      return task.value;
-    });
-    if (allfieldFilled == false) {
+    if (allGoals[circle.nextElementSibling.id].name === "") {
       errorMsg.classList.add("error-shown");
-    } else {
-      errorMsg.classList.remove("error-shown");
-      circle.parentElement.classList.toggle("completed");
-      allGoals[circle.nextElementSibling.id].completed =
-        !allGoals[circle.nextElementSibling.id].completed;
-      completedGoalsCount = Object.values(allGoals).filter(
-        (goal) => goal.completed
-      ).length;
-      innerStatusBar.style.width = `${completedGoalsCount / 3 * 100}%`;
-      quotes.forEach((quote,i) => { 
-        inspiringTexts.textContent = quotes[completedGoalsCount];
-      })
-      if (completedGoalsCount == 0) {
-        doneNumber.textContent = "";
-      }
-      doneNumber.textContent = `${completedGoalsCount}/3 completed`;
-
-      localStorage.setItem("allGoals", JSON.stringify(allGoals));
+      return;
     }
+    circle.parentElement.classList.toggle("completed");
+    allGoals[circle.nextElementSibling.id].completed =
+      !allGoals[circle.nextElementSibling.id].completed;
+
+    progressBarFun();
+
+    let quoteIndex = Math.min(
+      quotes.length - 1,
+      Math.floor((completedGoalsCount / totalGoals) * (quotes.length - 1))
+    );
+    inspiringTexts.textContent = quotes[quoteIndex];
+    if (completedGoalsCount == 0) {
+      doneNumber.textContent = "";
+    }
+
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
   });
 });
 taskTexts.forEach((text, i) => {
-  
   if (!allGoals[text.id]) {
     allGoals[text.id] = { name: "", completed: false };
   } else {
-  text.value = allGoals[text.id].name;
+    text.value = allGoals[text.id].name;
   }
   if (allGoals[text.id].completed) {
     text.parentElement.classList.add("completed");
@@ -59,19 +62,24 @@ taskTexts.forEach((text, i) => {
   text.addEventListener("focus", () => {
     errorMsg.classList.remove("error-shown");
   });
- 
+
   text.addEventListener("input", () => {
     if (allGoals[text.id].completed == true) {
       text.value = allGoals[text.id].name;
       return;
-      
-     }
+    }
     allGoals[text.id] = {
       name: text.value,
       completed: false,
     };
 
+    progressBarFun();
+
+    let quoteIndex = Math.min(
+      quotes.length - 1,
+      Math.floor((completedGoalsCount / totalGoals) * (quotes.length - 1))
+    );
+    inspiringTexts.textContent = quotes[quoteIndex];
     localStorage.setItem("allGoals", JSON.stringify(allGoals));
-   
   });
 });
